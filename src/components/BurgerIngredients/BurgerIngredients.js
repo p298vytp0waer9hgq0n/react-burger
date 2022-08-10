c3829bf2611db808d97d25da163528472cfaf368
p298vytp0waer9hgq0n <98211887+p298vytp0waer9hgq0n@users.noreply.github.com>
2022-08-10 15:30:41 +0500

    feat: ingredients scroll

--- src/components/BurgerIngredients/BurgerIngredients.js
+++ src/components/BurgerIngredients/BurgerIngredients.js
@@ -1,12 +1,16 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
 import Ingredient from "./Ingredient";
 
 import styles from './styles.module.css';
 
 export default function BurgerIngredients (props) {
+  useEffect(() => {
+    const ele = document.getElementById(current);
+    ele.scrollIntoView({behavior: 'smooth', block: 'start'});
+  });
+  const [current, setCurrent] = useState('buns');
   const titleIngrStyle = `${styles.burger__comps} text text_type_main-medium mt-2 mb-6`;
-  const [current, setCurrent] = React.useState('buns');
   const elements = props.data.map((item) => {
     return <Ingredient key={item._id} {...item} quantity={props.test.reduce((count, cur) => {
       return (cur._id === item._id) ? count + 1 : count;
@@ -25,11 +29,11 @@ export default function BurgerIngredients (props) {
         <Tab value="stuff" active={current === 'stuff'} onClick={setCurrent}>Начинки</Tab>
       </div>
       <div className={`${styles.burger__container} custom-scroll mt-8`}>
-        <h2 className={titleIngrStyle}>Булки</h2>
+        <h2 className={titleIngrStyle} id="buns">Булки</h2>
         {bunsElements}
-        <h2 className={titleIngrStyle}>Соусы</h2>
+        <h2 className={titleIngrStyle} id="sauces">Соусы</h2>
         {sauceElements}
-        <h2 className={titleIngrStyle}>Начинки</h2>
+        <h2 className={titleIngrStyle} id="stuff">Начинки</h2>
         {mainElements}
       </div>
     </section>
