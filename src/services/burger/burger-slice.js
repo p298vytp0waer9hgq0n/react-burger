9454900faa2ca7930614881f1c864076408f87bd
p298vytp0waer9hgq0n <98211887+p298vytp0waer9hgq0n@users.noreply.github.com>
2022-09-12 17:52:12 +0500

    refactor: косметика, второй кандидат

--- src/services/burger/burger-slice.js
+++ src/services/burger/burger-slice.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { ingTypes } from "../../utils/constants";
+import { ingrTypes } from "../../utils/constants";
 
 const initialState = {
   bun: {},
@@ -12,7 +12,7 @@ export const burgerSlice = createSlice({
   initialState,
   reducers: {
     burgerAdd: (state, action) => {
-      if (action.payload.type === ingTypes.bun) {
+      if (action.payload.type === ingrTypes.bun) {
         state.bun = action.payload;
       } else {
         state.ingredients.push(action.payload);
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,7 +8,7 @@ export const dragTypes = {
   sorting: 'ingredient/sort'
 }
 
-export const ingTypes = {
+export const ingrTypes = {
   bun: 'bun',
   sauce: 'sauce',
   main: 'main'
