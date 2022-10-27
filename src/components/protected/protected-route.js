da4a25e60f2f159985af4133a2104d4e1f514561
p298vytp0waer9hgq0n <98211887+p298vytp0waer9hgq0n@users.noreply.github.com>
2022-10-27 22:29:03 +0500

    refactor: changed conditional in ProtectedRoute now works based on refToken in cookie

--- src/components/protected/protected-route.js
+++ src/components/protected/protected-route.js
@@ -2,12 +2,15 @@ import { useEffect } from "react";
 import { Redirect, Route, useLocation } from "react-router-dom";
 import { useAuth } from "../../hooks/use-auth";
 import LoadingPage from "../../pages/loading";
+import { getCookie } from "../../utils/get-cookie";
 
 import PropTypes from 'prop-types';
 
 export default function ProtectedRoute ({ children, path, auth, redirect, ...rest }) {
   const { getUser, user } = useAuth();
   const location = useLocation();
+
+  const hasAuth = Boolean(getCookie('refToken'));
 
   useEffect(() => {
     getUser();
@@ -30,7 +33,7 @@ export default function ProtectedRoute ({ children, path, auth, redirect, ...res
 
   return (
     <Route path={path} {...rest} >
-      { Boolean(user.userName) === auth ? children : <Redirect to={to} /> }
+      { hasAuth === auth ? children : <Redirect to={to} /> }
     </Route>
   );
 }
